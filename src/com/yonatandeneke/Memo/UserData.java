4e63c3086f0dcfd588ea57d1dea215c87fad484e
Yonatan <yonatandeneke@gmail.com>
2022-08-13 14:56:25 -0500

    Changes

--- src/com/yonatandeneke/Memo/UserData.java
+++ src/com/yonatandeneke/Memo/UserData.java
@@ -2,16 +2,42 @@ package com.yonatandeneke.Memo;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class UserData {
 
-    private String folder;
+    private Path folder;
 
     public UserData(){
-        folder = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+        folder = Paths.get(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "\\Memo\\");
+        init();
     }
 
     public void init(){
+        if (!Files.exists(folder)) {
+            try {
+                Files.createDirectory(folder);
+                Files.write(Paths.get(folder + "\\options.txt"), Arrays.asList("name: You", "theme: light"), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void updateName(String name){
 
     }
+
+    public void updateTheme(String theme){
+
+    }
+
 }
